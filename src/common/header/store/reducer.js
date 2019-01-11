1ce52762b98a65ce4bdcde7dd619146881359d71
rick-liyue-huang <rick.liyue.huang@gmail.com>
2019-01-11 14:00:17 +1100

    import immutable

--- src/common/header/store/reducer.js
+++ src/common/header/store/reducer.js
@@ -1,22 +1,33 @@
 
+
+/*
+	immutable.js => get immutable object
+*/
+import { fromJS } from 'immutable';
 import * as actionTypes from './actionTypes';
 
-const defaultState = {
+const defaultState = fromJS({
 	focused: false
-};
+});
 
 export default (state = defaultState, action) => {
 	
 	if (action.type === actionTypes.SEARCH_FOCUS) {
-		return {
+		/*return {
 			focused: true
-		}
+		}*/
+		// immutable object set methods 
+		// will combine the previous immutable object and set value,
+		// to return the new object.
+		// remember that: not return the old object
+		return state.set('focused', true) 
 	}
 
 	if (action.type === actionTypes.SEARCH_BLUR) {
-		return {
+		/*return {
 			focused: false
-		}
+		}*/
+		return state.set('focused', false)
 	}
 
 	return state;
